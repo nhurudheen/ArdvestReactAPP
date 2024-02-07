8ff94de5b134c3a491fb24c50493c34b2e3056db
nhurudheen <ifaniyioluwapelumi@gmail.com>
2024-02-07 18:13:07 +0100

    Investment List

--- src/Pages/user/pages/investment.js
+++ src/Pages/user/pages/investment.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import NavigationHeader from "../../../Components/navigationHeader";
 import { useInvestmentTypeList, useInvestmentTypes } from "../userLayout/reusableEffects";
 import comingSoon from "../../../assets/icons/comingSoon.svg";
+import investmentImage from "../../../assets/images/cowThumbnail.png";
 import Spinner from "../../../Components/spinner";
 import { useSelector } from "react-redux";
+import Modal from "../../../Components/modals";
 
 const Investment = ({ setPageTitle }) => {
     useEffect(() => {
@@ -15,6 +17,7 @@ const Investment = ({ setPageTitle }) => {
     const investmentTypes = useInvestmentTypes();
     const [selectedInvestmentType, setSelectedInvestmentType] = useState(null);
     const [selectedInvestmentName, setSelectedInvestmentName] = useState(null);
+    const [selectedInvestmentDetails, setSelectedInvestmentDetails] = useState(null);
     const investmentTypeDetails = useInvestmentTypeList(selectedInvestmentType);
     useEffect(() => {
         if (investmentTypes.length > 0) {
@@ -23,7 +26,7 @@ const Investment = ({ setPageTitle }) => {
         }
     }, [investmentTypes])
 
-    const userSelectedInvestmentType =(investmentTypeId, investmentTypeName)=>{
+    const userSelectedInvestmentType = (investmentTypeId, investmentTypeName) => {
         setSelectedInvestmentName(investmentTypeName);
         setSelectedInvestmentType(investmentTypeId);
     }
@@ -44,7 +47,7 @@ const Investment = ({ setPageTitle }) => {
                             <div id="tabs" className="flex gap-3 text-sm text-white md:mt-8">
                                 {investmentTypes.map((val, key) => {
                                     return (
-                                        <button className={`tab-btn active px-6 py-2 rounded bg-primary ${selectedInvestmentType === val.investmentTypeId ? 'opacity-50' : ''}`} onClick={() => userSelectedInvestmentType(val.investmentTypeId, val.investmentName)} key={key}>{val.investmentName} </button>
+                                        <button className={`tab-btn active px-6 py-2 rounded bg-primary ${selectedInvestmentType === val.investmentTypeId ? '' : 'opacity-50'}`} onClick={() => userSelectedInvestmentType(val.investmentTypeId, val.investmentName)} key={key}>{val.investmentName} </button>
                                     )
                                 })}
                             </div>
@@ -53,10 +56,26 @@ const Investment = ({ setPageTitle }) => {
                             <div className="tab-content p-3 md:p-5 rounded-md">
                                 {investmentTypeDetails.length > 0
                                     ?
-                                    (
-                                        <div>
-
-                                        </div>
+                                    (<div>
+                                        {investmentTypeDetails.map((val, key) => {
+                                            return (
+                                                <div className="grid md:flex justify-between items-center bg-[#f8f8f8] px-4 py-6 rounded mb-6 gap-8" key={key}>
+                                                    <div className="flex items-center gap-3">
+                                                        <img src={investmentImage} alt="" />
+                                                        <div className="grid">
+                                                            <span className="text-lg font-semibold text-primary">{val.investmentName}</span>
+                                                            <span className={`text-sm ${(val.status === 'Active') ? 'text-primary/50' : 'text-red-500'} font-medium`}>{val.status} </span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="grid grid-cols-2 md:flex gap-4">
+                                                        <div>
+                                                            <button className="text-sm px-6 py-3 rounded-md bg-primary text-white w-full" onClick={()=>setSelectedInvestmentDetails(val)}>{(val.status === 'Active') ? 'Invest Now' : 'View More'}</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
                                     )
                                     :
                                     (
@@ -74,6 +93,58 @@ const Investment = ({ setPageTitle }) => {
                             </div>
                         )}
 
+                        {
+                            selectedInvestmentDetails && (
+                                <div>
+                                    <Modal isVisible={selectedInvestmentDetails !== null} onClose={()=>setSelectedInvestmentDetails(null)}>
+                                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                            <div className="rounded-lg overflow-hidden">
+                                                <img src={selectedInvestmentDetails.investmentImage} alt="" className="h" />
+                                            </div>
+                                            <div>
+                                                <p className="text-lg font-semibold text-primary">{selectedInvestmentDetails.investmentName}</p>
+                                                <p className="text-sm text-[#c4c4c4] text-justify">{selectedInvestmentDetails.description}</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="grid grid-cols-2 md:grid-cols-1 md:flex justify-between text-[#c4c4c4] mt-6 gap-y-3">
+                                            <div>
+                                                <p className="text-xs text-black">Current investors</p>
+                                                <p className="font-semibold text-black">{selectedInvestmentDetails.noOfPeople}</p>
+                                            </div>
+                                            <div>
+                                                <p className="text-xs text-black">Minimum Investment</p>
+                                                <p className="font-semibold text-black">&#8358;{selectedInvestmentDetails.minimumInvestment}</p>
+                                            </div>
+                                            <div>
+                                                <p className="text-xs text-black">Maximum Investment</p>
+                                                <p className="font-semibold text-black">&#8358;{selectedInvestmentDetails.maximumInvestment}</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="grid grid-cols-2 md:grid-cols-1 md:flex justify-between text-[#c4c4c4] mt-6 gap-y-3">
+                                            <div>
+                                                <p className="text-xs text-black">ROI</p>
+                                                <p className="font-semibold text-black">{selectedInvestmentDetails.roi} %</p>
+                                            </div>
+                                            <div>
+                                                <p className="text-xs text-black">Start Date</p>
+                                                <p className="font-semibold text-black">&#8358;{selectedInvestmentDetails.startDate}</p>
+                                            </div>
+                                            <div>
+                                                <p className="text-xs text-black">End Date</p>
+                                                <p className="font-semibold text-black">&#8358;{selectedInvestmentDetails.endDate}</p>
+                                            </div>
+                                        </div>
+
+                                        <p className="text-sm mt-4 m-8">Investment Status: <span className={`font-semibold ${(selectedInvestmentDetails.status === 'Active') ? 'text-primary/50' : 'text-red-500'}`}>{selectedInvestmentDetails.status}</span></p>
+                                        {(selectedInvestmentDetails.status === 'Active')
+                                            ? <button class='bg-primary text-center p-4 text-sm rounded text-white w-full'>Invest now</button> : ''}
+
+                                    </Modal>
+                                </div>
+                            )
+                        }
                     </div>
                 )
                 : (
@@ -88,7 +159,7 @@ const Investment = ({ setPageTitle }) => {
                     </div>
                 )
             }
-        </div>
+        </div >
     );
 }
 
